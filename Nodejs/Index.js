637873b373fd9fdea8056f2ba01b54bb191840af
Vigneshprabu <155611674+VIKYPRU@users.noreply.github.com>
2024-05-09 12:34:43 +0530

    09.05.24

--- Nodejs/Index.js
+++ Nodejs/Index.js
@@ -42,7 +42,142 @@
 
 
 //4 and 5
-const{add,sub,div}=require('./Math')
-add(2,6)
-sub(5,5)
-div(5,8)
+// const{add,sub,div}=require('./Math')
+// add(2,6)
+// sub(5,5)
+// div(5,8)
+
+
+//writting the file for create and update
+
+// const fs=require('node:fs')
+//  fs.writeFileSync('./data.txt','hello world')
+//  fs.writeFile('./data.txt','hi world',{flag:"a"},(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//         else{
+//             console.log("file writen successfully")
+//         }
+        
+//     }
+//  )
+
+ //to delete
+
+//  const fs=require('node:fs')
+//  fs.unlink('./data.txt',(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("deleted successfully")
+//     }
+//  })
+
+ //to transfer the file
+
+//  const fs=require('node:fs')
+//  const readableStream=fs.createReadStream('./file1.txt',{
+//     encoding:'utf-8',
+//     highWaterMark:2 //=>to output show the 2 world
+
+//  })
+//  const writeableStream=fs.createWriteStream('./file2.txt')
+//  readableStream.on("data",(chunk)=>{
+//     console.log(chunk)
+//     writeableStream.write(chunk)
+//  })
+
+ //to transfer file using pipes
+
+//  const fs=require('node:fs')
+//  const zlib=require('zlib')
+//  const zip=zlib.createGzip()
+//  const readableStream=fs.createReadStream('./file1.txt',{
+//     encoding:'utf-8',
+//     highWaterMark:3
+// })
+// readableStream.pipe(zip).pipe(fs.WriteStream('./file3.txt.zip'))
+
+//creating http server
+
+// const http=require('node:http')
+// const server=http.createServer((req,res)=>{
+//     res.writeHead(200,{        //200 is keyword no for success
+//         "content-type":"text./plan"
+//     })
+//     res.end("hello world")
+// })
+// server.listen(3001,()=>{
+//     console.log('server is running on port 3001')
+// })
+
+//json link with content
+
+// const http=require('node:http')
+// const json={
+//     "name":"adam",
+//     "age":25, 
+//     "addre":"abc"
+// }
+// const server=http.createServer((req,res)=>{
+//     res.writeHead(200,{
+//         "content-type":"application/json"
+//     })
+//     res.end(JSON.stringify(json))
+// })
+// server.listen(3002,()=>{
+//     console.log(json)
+// })
+
+//html in json
+
+const http=require('node:http')
+const fs=require('node:fs')
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{
+        "content-type":"text/html"
+    })
+    fs.createReadStream('./index.html').pipe(res)
+})
+server.listen(3002,()=>{
+    console.log("server is running on port 3002")
+})
+
+//routing in server
+
+// const http=require('node:http')
+// const server=http.createServer((req,res)=>{
+
+
+//     if(req.url === '/'){
+//         res.writeHead(200,{
+//             "content-type":"text/plain"
+//         })
+//         res.end("Homepage")
+//     }
+//     else if(req.url === '/about'){
+//         res.writeHead(200,{
+//             "content-type":"text/plain"
+//         })
+//         res.end("about page")
+//     }
+//     else if(req.url === '/api'){
+//         res.writeHead(200,{
+//             "content":"text/json"
+//         })
+//         res.end(JSON.stringify({
+//             "name":"adam",
+//             "age":10,
+        
+//         }))
+//     }
+//     else{
+//         res.writeHead(404)
+//         res.end("page not found")
+//     }
+// })
+// server.listen(3000,()=>{
+//     console.log("server is running on  port 3000")
+// })
